bff7262557d25181f880910d1cb24ad5ccb6a6a7
Alex Gurr <thegurrkin@hotmail.co.uk>
2017-11-01 16:16:35 +0000

    Initial Commit
    React wrapper for conversation form + tests

---
+++ FormChat.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _once from 'lodash/once';
+// import cf from 'conversational-form';
+import './styles/_form-chat.scss';
+import { BT_ICON, USER_ICON } from './assets/icons';
+
+const RESERVED_IDS = ['chat', 'chatForm'];
+const RESERVED_ERROR = `Can't instantiate a new FormChat with a question/question ID equalling (${RESERVED_IDS.join(', ')}).`;
+const REF_SUFFIX = 'Ref';
+const INPUT_TYPE = 'input';
+const SELECT_TYPE = 'select';
+
+/**
+ * Check if the IDs for the question props are all valid. They must be unique, and not-reserved
+ *
+ * @param {string} id
+ * @param {Object} state
+ *
+ * @return {void}
+ */
+function checkIfIdIsReserved(id, state) {
+  if (RESERVED_IDS.includes(id)) {
+    throw new Error(RESERVED_ERROR);
+  }
+
+  if (Object.keys(state).includes(id)) {
+    throw new Error(`All question IDs must be unique (${id}).`);
+  }
+}
+
+/**
+ * Returns the ID with the suffix appended
+ *
+ * @param {string} id
+ *
+ * @return {string}
+ */
+function getRefKey(id) {
+  return `${id}${REF_SUFFIX}`;
+}
+
+/**
+ * Check every el/ref in state has been assigned
+ *
+ * @param {Object} state
+ *
+ * @return {boolean}
+ */
+function allRefsSet(state) {
+  return Object.keys(state)
+    .every(stateKey => !stateKey.includes(REF_SUFFIX) || Boolean(state[stateKey]));
+}
+
+export default class FormChat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = props.questions.reduce((state, question) => {
+      checkIfIdIsReserved(question.id, state);
+
+      return {
+        ...state,
+        [question.id]: '',
+        [getRefKey(question.id)]: void 0
+      };
+    }, { chatFormRef: void 0, chat: void 0 });
+
+    this.setRef = this.setRef.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.renderFormComponent = this.renderFormComponent.bind(this);
+    this.startConversation = _once(this.startConversation);
+  }
+
+  onSubmit() {
+    const { questions, onSubmit, chatOptions: { submittedResponseText } } = this.props;
+
+    if (submittedResponseText) {
+      window.ConversationalForm.addRobotChatResponse(submittedResponseText);
+    }
+
+    const formValues = questions.reduce((submitValues, question) => ({
+      ...submitValues, [question.id]: this.state[question.id]
+    }), {});
+
+    onSubmit(formValues);
+  }
+
+  /**
+   * Set an element ref in state
+   *
+   * @param {string} elName
+   *
+   * @return {void}
+   */
+  setRef(elName) {
+    const tryToStartConversation = () => {
+      if (allRefsSet(this.state)) {
+        this.startConversation();
+      }
+    };
+
+    return (el) => {
+      if (el && !this.state[elName]) {
+        this.setState({ [elName]: el }, tryToStartConversation);
+      }
+    };
+  }
+
+  /**
+   * Try and start the form chat. If all refs haven't been set, then do nothing. Set questions from
+   * props and configure form chat.
+   *
+   * @return {void}
+   */
+  startConversation() {
+    const {
+      questions,
+      onStepCallback,
+      chatOptions: {
+        robotResponseTime,
+        robotChainResponseTime,
+        showUserThinking,
+        thankTheUser,
+        introText
+      }
+    } = this.props;
+    const { chatFormRef } = this.state;
+
+    questions.forEach((question, index) => {
+      const displayThanks = thankTheUser && thankTheUser.includes(question.id) ? 'Thanks.&&' : '';
+      const questionPrefix = index === 0 && introText ? `${introText}&&` : displayThanks;
+      const questionText = `${questionPrefix}${question.question}`;
+
+      this.state[getRefKey(question.id)].setAttribute('cf-questions', questionText);
+    });
+
+    this.setState({ chat: window.cf.ConversationalForm.startTheConversation({
+      formEl: chatFormRef,
+      robotImage: BT_ICON,
+      userImage: USER_ICON,
+      userInterfaceOptions: {
+        robot: {
+          robotResponseTime: robotResponseTime || 250,
+          chainedResponseTime: robotChainResponseTime || 1500
+        },
+        user: { showThinking: showUserThinking || false }
+      },
+      context: document.getElementById('cf-context'),
+      flowStepCallback: ({ tag: { id }, text }, success, error) => {
+        // Set the result of the input in state
+        this.setState({ [id]: text });
+
+        // Call the callback prop function
+        onStepCallback({ id, text, success, error });
+      },
+      submitCallback: this.onSubmit
+    }) });
+  }
+
+  /**
+   * Get the appropriate element for each form question in props
+   *
+   * @param {Object} question
+   * @return {JSX}
+   */
+  renderFormComponent(question) {
+    const ref = getRefKey(question.id);
+
+    if (question.componentType === INPUT_TYPE) {
+      return (
+        <input key={question.id} id={question.id} type={question.type} ref={this.setRef(ref)} />
+      );
+    }
+
+    return (
+      <select key={question.id} ref={this.setRef(ref)} type={question.type} id={question.id}>
+        {question.selectOptions && question.selectOptions.map(selectOption => (
+          <option
+            key={selectOption.value}
+            value={selectOption.value}
+          >
+            {selectOption.label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+  render() {
+    const { questions } = this.props;
+
+    return (
+      <div className="form-chat">
+        <div id="cf-context" >
+          <form id="form" className="form" ref={this.setRef('chatFormRef')}>
+            {questions.map(this.renderFormComponent)}
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+FormChat.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onStepCallback: PropTypes.func,
+  questions: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    componentType: PropTypes.oneOf([INPUT_TYPE, SELECT_TYPE]).isRequired,
+    type: PropTypes.string,
+    question: PropTypes.string.isRequired,
+    selectOptions: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string
+    }))
+  })).isRequired,
+  chatOptions: PropTypes.shape({
+    robotResponseTime: PropTypes.number,
+    robotChainResponseTime: PropTypes.number,
+    showUserThinking: PropTypes.bool,
+    thankTheUser: PropTypes.arrayOf(PropTypes.string),
+    introText: PropTypes.string,
+    submittedResponseText: PropTypes.string
+  })
+};
+
+FormChat.defaultProps = {
+  onStepCallback: ({ success }) => { success(); },
+  chatOptions: {}
+};
---
+++ FormChat.spec.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import FormChat from './FormChat';
+
+const MOCK_QUESTIONS = [
+  { id: 'foo', componentType: 'input', question: 'Foo?' },
+  { id: 'bar', componentType: 'select', question: 'Bar?', selectOptions: [{ label: 'foo', value: 'foo' }] }
+];
+
+const MOCK_CONVERSATIONAL_FORM = {
+  ConversationalForm: {
+    startTheConversation: () => {}
+  }
+};
+
+function countMatchingElements(formChat, elName) {
+  let count = 0;
+
+  formChat.find(elName).forEach(() => {
+    count += 1;
+  });
+
+  return count;
+}
+
+function renderFormChat(props = {}, cf = MOCK_CONVERSATIONAL_FORM) {
+  window.cf = cf;
+
+  const formChat = mount(
+    <FormChat
+      onSubmit={() => {}}
+      questions={MOCK_QUESTIONS}
+      {...props}
+    />
+  );
+
+  expect(formChat.find('.form-chat')).toBePresent();
+  expect(formChat.find('#cf-context')).toBePresent();
+  expect(formChat.find('input')).toBePresent();
+  expect(formChat.find('select')).toBePresent();
+
+  expect(countMatchingElements(formChat, 'option')).toBe(1);
+
+  const state = formChat.state();
+  const stateKeys = Object.keys(state);
+
+  expect(stateKeys.includes('fooRef')).toBe(true);
+  expect(stateKeys.includes('barRef')).toBe(true);
+  expect(state.foo).toEqual('');
+  expect(state.bar).toEqual('');
+
+  return formChat;
+}
+
+describe('common/components/FormChat', () => {
+  test('should be able to render the form chat as per the snapshot', () => {
+    expect(renderFormChat()).toMatchSnapshot();
+  });
+
+  test('should set all refs', () => {
+    const formChat = renderFormChat();
+    const state = formChat.state();
+    const refsSet = Object.keys(formChat.state())
+      .every(key => !key.includes('Ref') || Boolean(state[key]));
+
+    expect(refsSet).toBe(true);
+  });
+
+  test('should throw exception when using reserved IDs', () => {
+    expect(() => renderFormChat({
+      questions: [{ id: 'chat', componentType: 'input', question: 'foo' }]
+    })).toThrow('Can\'t instantiate a new FormChat with a question/question ID equalling (chat, chatForm).');
+  });
+
+  test('should throw exception when using duplicate IDs', () => {
+    expect(() => renderFormChat({
+      questions: [
+        { id: 'foo', componentType: 'input', question: 'foo' },
+        { id: 'foo', componentType: 'input', question: 'foo' }
+      ]
+    })).toThrow('All question IDs must be unique (foo).');
+  });
+
+  test('should try to start the form chat', () => {
+    const START = jest.fn(() => true);
+    const CF = {
+      ConversationalForm: {
+        startTheConversation: START
+      }
+    };
+
+    renderFormChat(void 0, CF);
+
+    expect(START.mock.calls.length).toBe(1);
+    expect(START.mock.calls[0][0].formEl).toBeTruthy();
+    expect(START.mock.calls[0][0].robotImage).toBeTruthy();
+    expect(START.mock.calls[0][0].userImage).toBeTruthy();
+    expect(START.mock.calls[0][0].userInterfaceOptions).toBeTruthy();
+    expect(START.mock.calls[0][0].flowStepCallback).toBeTruthy();
+    expect(START.mock.calls[0][0].submitCallback).toBeTruthy();
+  });
+
+  test('should be able to set chat options', () => {
+    const START = jest.fn(() => true);
+    const CF = {
+      ConversationalForm: {
+        startTheConversation: START
+      }
+    };
+
+    const formChat = renderFormChat({ chatOptions: {
+      robotResponseTime: 1,
+      robotChainResponseTime: 2,
+      showUserThinking: true,
+      thankTheUser: ['bar'],
+      introText: 'intro'
+    } }, CF);
+
+    const firstQuestion = formChat.state().fooRef.getAttribute('cf-questions');
+    const secondQuestion = formChat.state().barRef.getAttribute('cf-questions');
+
+    expect(firstQuestion.includes('intro')).toBe(true);
+    expect(secondQuestion.includes('intro')).toBe(false);
+    expect(secondQuestion.includes('Thanks')).toBe(true);
+
+    expect(START.mock.calls.length).toBe(1);
+    expect(START.mock.calls[0][0].userInterfaceOptions).toBeTruthy();
+    expect(START.mock.calls[0][0].userInterfaceOptions.robot.robotResponseTime).toEqual(1);
+    expect(START.mock.calls[0][0].userInterfaceOptions.robot.chainedResponseTime).toEqual(2);
+    expect(START.mock.calls[0][0].userInterfaceOptions.user.showThinking).toEqual(true);
+  });
+
+  test('should be able to submit', () => {
+    const SUBMIT = jest.fn();
+    const ADD_RESPONSE = jest.fn();
+
+    window.ConversationalForm = { addRobotChatResponse: ADD_RESPONSE };
+
+    const formChat = renderFormChat({
+      onSubmit: SUBMIT,
+      chatOptions: { submittedResponseText: 'foo' }
+    });
+
+    formChat.instance().onSubmit();
+
+    expect(SUBMIT.mock.calls.length).toBe(1);
+    expect(ADD_RESPONSE.mock.calls.length).toBe(1);
+    expect(SUBMIT.mock.calls[0][0]).toEqual({ foo: '', bar: '' });
+  });
+});
